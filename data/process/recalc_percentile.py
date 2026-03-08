8810acf09e7ecff435c944f15ea7e98f28805f0f
calista <cal1sta17.030@gmail.com>
2026-03-08 10:20:46 -0700

    Restructure: Move files out of project folder

--- data/process/recalc_percentile.py
+++ data/process/recalc_percentile.py
@@ -59,5 +59,5 @@ def recalculate_percentiles(csv_path):
 
 
 if __name__ == "__main__":
-    csv_path = "/Users/dontwakemeup/Desktop/Code/272project/272Project/project/data/process/merged_anime_final.csv"
+    csv_path = "/Users/dontwakemeup/Desktop/Code/272project/272Project/data/process/merged_anime_final.csv"
     recalculate_percentiles(csv_path)
